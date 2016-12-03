fbc9c142ad03484601b298cb3dbf497ae1809b5d
Krzysztof Kraszewski <krszwsk@gmail.com>
2016-12-03 21:28:01 +0100

    deleted old files, creating new soon :upside_down_face

--- vim/setup.sh
+++
@@ -1,8 +0,0 @@
-sudo apt-get update
-sudo apt install -y vim
-sudo apt install -y git
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vi
-git clone https://github.com/krszwsk/dotfiles/ ~/.vim/dotfiles
-rm ~/.vimrc
-ln -s ~/.vim/dotfiles/vim/.vimrc ~/.vimrc
-echo "Run :PluginInstall in Vim to finish configuration"
