9356e357eeeacdda3d25464bc92f4982502d0771
Krzysztof Kraszewski <krszwsk@gmail.com>
2017-05-11 18:42:16 +0200

    js dotfiles

---
+++ js/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "rules": {
+      "jsx-a11y/img-has-alt": 0
+    }
+};
