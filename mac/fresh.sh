fb83b152def7b1dca042e1b83d70c7f88521751a
Krzysztof Kraszewski <krzysztof@kraszewski.app>
2018-10-08 16:02:16 +0200

    Update fresh.sh

--- mac/fresh.sh
+++ mac/fresh.sh
@@ -66,6 +66,7 @@ pip3 install awscli --upgrade --user
 rm ~/Library/Application\ Support/Code/User/settings.json
 code --install-extension zhuangtongfa.material-theme
 code --install-extension isudox.vscode-jetbrains-keybindings
+code --install-extension eamodio.gitlens
 cp ~/Projects/Personal/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 
 # Powerline fonts
